3d563e46914872ab781815a415904d4b0347f9b7
ChanceWu <1565222098@qq.com>
2023-05-29 12:00:35 +0800

    bug:输入框只能输入单个中文字符的问题

--- src/components/common/ComInput/index.tsx
+++ src/components/common/ComInput/index.tsx
@@ -1,18 +1,19 @@
 import { Input, InputProps } from 'antd';
-import React from 'react'
+import React from 'react';
 
 interface ComInputProps extends InputProps {
-    value?: string;
-    onChange?: (v: any) => void;
+  value?: string;
+  onChange?: (v: any) => void;
 }
-const ComInput:React.FC<ComInputProps> = ({ onChange, ...rest}) => {
-    const changeHandle = (e: any) => {
-        const val = e.target.value;
-        onChange?.(val.replace(/[^\u4E00-\u9FA5A-Za-z0-9\.]/g, ''));
-    }
-  return (
-    <Input onChange={changeHandle} {...rest} />
-  )
-}
+const ComInput: React.FC<ComInputProps> = ({ onChange, ...rest }) => {
+  const changeHandle = (e: any) => {
+    const val = e.target.value;
+    const newVal = val.replace(/[^\u4E00-\u9FA5A-Za-z0-9\'\.]/g, '');
+    console.log('input', val, newVal);
+    onChange?.(newVal);
+    // onChange?.(val);
+  };
+  return <Input {...rest} onChange={changeHandle} />;
+};
 
-export default ComInput
+export default ComInput;
